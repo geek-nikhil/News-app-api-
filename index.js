4eaf9a73e8b35539263c372f4126c18acda67d94
Nikhil <nikhilraikwar846@gmail.com>
2023-09-25 17:46:52 +0530

    searching option has been added and refreshing issue also solved

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 let k=0;
 let i=0;
 var t=0;
+var sea=document.getElementById(cardContainer);
+let parentElement = document.getElementById('cardContainer');
+var query = "latest news";
+  function search(){
+        parentElement.innerHTML="";
+     query = document.getElementById("search-input").value;
+    console.log(query); 
+    checknews(query);
+    
+  }
+sea ="";
 var cardData = [
     { title: "Card " + t, content: "This is the content of Card 1." },
-   
 ];
   function myFunction() {
     var x = document.getElementById("myNavbar");
@@ -12,30 +22,30 @@ var cardData = [
     } else {
         x.className = "navbar";
     }
-}
+  }
 
 const key="4fe360bea1754332b25594ca6cc98ccc";
-const api = "https://newsapi.org/v2/top-headlines?country=in&apiKey=4fe360bea1754332b25594ca6cc98ccc";
-async function checknews(){
+const api = "https://newsapi.org/v2/everything?q="+`${query}`+"&apiKey=4fe360bea1754332b25594ca6cc98ccc";
+async function checknews(query){
+    sea.innerHTML="";
+    const api = "https://newsapi.org/v2/everything?q="+`${query}`+"&apiKey=4fe360bea1754332b25594ca6cc98ccc";
     const response = await fetch(api + `&appid=${key}`);
     var data = await response.json();
     console.log(data);
     console.log(data.articles.length)
     k=data.articles.length;
     for(i=0;i<k;i++){
+        if(data.articles[i].description==null){
+            continue;
+        }
     cardData = [
-            { title: data.articles[t].title, content: data.articles[t].description ,image: data.articles[t].urlToImage},
+            { title: data.articles[i].title, content: data.articles[i].description ,image: data.articles[i].urlToImage},
         ];
         addCardsToContainer(cardData);
     t++;
     }
 }
 console.log(k);
-checknews();
-    // Sample data for cards
-
-
-    // Function to create a card element
     function createCard(title, content,image) {
         const card = document.createElement("div");
         card.className = "card";
@@ -47,7 +57,6 @@ checknews();
         return card;
     }
 
-    // Function to add cards to the cardContainer
     function addCardsToContainer(data) {
         const cardContainer = document.getElementById("cardContainer");
         data.forEach(item => {
@@ -57,4 +66,5 @@ checknews();
 
     }
     // Add cards to the container
+    checknews(query);
   
